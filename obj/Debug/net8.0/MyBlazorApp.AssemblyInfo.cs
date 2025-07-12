46b8cf95ef20c553bb412f87aad36a4636ac0eef
Iska Mabo <maboiskanderr@gmail.com>
2025-07-13 04:12:30 +0600

    styles

--- obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1e55d921380a7c2ed5d19ac0a39ceb8022fcf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35fb56a37699c42cd09a497d36a77bb59c6a4468")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
