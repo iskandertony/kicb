34321c6618976494ebe5703c6b342580e62b9357
Iska Mabo <maboiskanderr@gmail.com>
2025-07-14 00:30:51 +0600

    carusel cards

--- obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a64b2107e6115a6e360a038294f90933f7716ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aba9663763079d7ea4a82500c6b5da2301d5801")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
