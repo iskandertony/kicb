a8374c7cec22bdee9546d0946e999e316afc452e
Iska Mabo <maboiskanderr@gmail.com>
2025-07-13 21:26:51 +0600

    app bar styles

--- obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ddc6e2f2196385eb643803388dfa83398717fbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63efb9e196790b1cff7a8ae9df806ebc297378c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
