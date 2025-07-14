db3920bcd076d769922ba5a79756c6ab8e9a4a0f
Iska Mabo <maboiskanderr@gmail.com>
2025-07-14 15:38:06 +0600

    fix base url

--- obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ed11e03b6ee79ba30b1dfdfc6d1b69a49a26e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ccdaeca667abd0a574527b3d4f55378e3b304a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
