198ebe4d84cd3334879bd91fcc5c4d193df017a7
Iska Mabo <maboiskanderr@gmail.com>
2025-07-14 10:56:18 +0600

    Add Vercel config

--- obj/Release/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Release/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a1d2562642ccf8ff8363e808b8937f83c8416d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+424e085388056b9d7b040489685f099cf8daaad8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
