90a1d2562642ccf8ff8363e808b8937f83c8416d
Iska Mabo <maboiskanderr@gmail.com>
2025-07-14 06:08:07 +0600

    fix pallete theme

--- obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f0019480ac663b27bf60b59dd8defa92049d1a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8cf013b79e022bacf7eea8365f65c86be5d9a35")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
