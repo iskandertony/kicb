08ccdaeca667abd0a574527b3d4f55378e3b304a
Iska Mabo <maboiskanderr@gmail.com>
2025-07-14 12:16:48 +0600

    deploy: обновление gh-pages

--- obj/Release/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Release/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe3d8f91b06060b1b4ee359757fefe5ad8bd553")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ed11e03b6ee79ba30b1dfdfc6d1b69a49a26e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
